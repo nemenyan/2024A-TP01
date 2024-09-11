4f1f76754b53f87ff6c6ac1656916dd1f730127d
Yolaine Karemere <yolainekare@gmail.com>
2024-09-11 17:02:16 -0400

    essai

--- ex2.py
+++ ex2.py
@@ -1,4 +1,15 @@
 # TODO: Créer un script pour calculer les ressources nécessaires pour assainir la Seine.
 # TODO: Importer les modules nécessaires.
 
-water_quantity = ...
+water_quantity = int(input("Veuillez indiquer la quantité d'eau à assainir: "))
+print(water_quantity)
+x=
+x= 1
+y= 3
+z= 0.5
+
+if water_quantity== 10:
+filter == x*2
+UV_lamp == y*2
+chlore == z*2
+print("Voici les matériaux requis pour l'assainemenr de", water_quantity,"L d'eau:",filter,"filtres, -",UV_lamp,"lampes UV, -",chlore,"kg de chlore")
